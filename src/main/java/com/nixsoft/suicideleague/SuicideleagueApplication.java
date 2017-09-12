93b06402b19a88e8560d9ff6c32ec8865775a0d0
dnl0rot <david_nix@homedepot.com>
2017-09-12 19:23:10 -0400

    initial commit

---
+++ src/main/java/com/nixsoft/suicideleague/SuicideleagueApplication.java
@@ -0,0 +1,12 @@
+package com.nixsoft.suicideleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuicideleagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuicideleagueApplication.class, args);
+	}
+}
